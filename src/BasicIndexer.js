d6232d2567bcd0dffbdec693354050d85a452390
Jose Garcia <jogarcia@tuta.io>
2020-10-07 10:40:41 -0400

    Make simple BasicIndexer

--- src/BasicIndexer.js
+++ src/BasicIndexer.js
@@ -1,5 +1,16 @@
+const makeIndexedCollection = require("./IndexerFunctions")
+  .makeIndexedCollection;
+const toArray = require("./IndexerFunctions").toArray;
+
 class BasicIndexer {
-  constructor(params) {}
+  constructor(array, key = "id") {
+    this.index = key;
+    this.indexedCollection = makeIndexedCollection(array, key);
+  }
+
+  getArray() {
+    return toArray(this.indexedCollection, true, this.index);
+  }
 }
 
 module.exports = BasicIndexer;
